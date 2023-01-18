719019dd766a1b40b8c0f3861acdbcb17519dff6
OXcarXierra <littlestein@snu.ac.kr>
2023-01-18 10:25:04 +0900

    fix: editor now works in /write

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,91 +1,38 @@
-import React, { FunctionComponent, useEffect, useState } from 'react'
-import EditorJS, { ToolConstructable, ToolSettings } from '@editorjs/editorjs'
+import React, { memo, useEffect, useRef } from 'react'
+import EditorJS, { OutputData } from '@editorjs/editorjs'
 import { tools } from './tools'
 
-/**
- *
- * @param {EditorJS.Tool[]} toolsList
- * @param {*} param1
- * @param {EditorJS.EditorConfig} options
- */
+interface Props {
+  data?: OutputData
+  onChange(val: OutputData): void
+  holder: string
+}
 
-const useEditor = (
-  toolsList: { [toolName: string]: ToolConstructable | ToolSettings<any> },
-  { data, editorRef },
-  options: EditorJS.EditorConfig = {}
-) => {
-  const [editorInstance, setEditor] = useState<EditorJS | null>(null)
-  const { data: ignoreData, tools: ignoreTools, holder: ignoreHolder, ...editorOptions } = options
+const Editor = ({ data, onChange, holder }: Props) => {
+  const ref = useRef<EditorJS>() //add a reference to editor
 
-  // initialize
+  //initialize editorjs
   useEffect(() => {
-    // create instance
-    const editor = new EditorJS({
-      /**
-       * Id of Element that should contain the Editor
-       */
-      holder: 'editor-js',
-
-      /**
-       * Available Tools list.
-       * Pass Tool's class or Settings object for each Tool you want to use
-       */
-      tools: toolsList,
-
-      /**
-       * Previously saved data that should be rendered
-       */
-      data: data || {},
-
-      initialBlock: 'paragraph',
-
-      // Override editor options
-      ...editorOptions,
-    })
-
-    setEditor(editor)
-
-    // cleanup
+    //initialize editor if we don't have a reference
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: holder,
+        tools: tools,
+        data,
+        async onChange(api, event) {
+          const data = await api.saver.save()
+          onChange(data)
+        },
+      })
+      ref.current = editor
+    }
+    //add a return function handle cleanup
     return () => {
-      editor.isReady
-        .then(() => {
-          editor.destroy()
-          setEditor(null)
-        })
-        .catch((e) => console.error('ERROR editor cleanup', e))
+      if (ref.current && ref.current.destroy) {
+        ref.current.destroy()
+      }
     }
-  }, [toolsList])
-
-  // set reference
-  useEffect(() => {
-    if (!editorInstance) {
-      return
-    }
-    // Send instance to the parent
-    if (editorRef) {
-      editorRef(editorInstance)
-    }
-  }, [editorInstance, editorRef])
-
-  return { editor: editorInstance }
+  }, [])
+  return <div id={holder} />
 }
-
-const EditorContainer = ({ editorRef, children, data, options }) => {
-  useEditor(tools, { data, editorRef }, options)
-
-  return (
-    <React.Fragment>
-      {!children && <div className="container" id="editor-js"></div>}
-      {children}
-      <style jsx>{`
-        .container {
-          width: 100%;
-          border: 1px solid #c0c0c0;
-          padding: 2px 0;
-        }
-      `}</style>
-    </React.Fragment>
-  )
-}
-
-export { useEditor, EditorContainer }
+export default memo(Editor)
---
+++ src/pages/write.tsx
@@ -0,0 +1,16 @@
+import { OutputData } from '@editorjs/editorjs'
+import type { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+// important that we use dynamic loading here
+// editorjs should only be rendered on the client side.
+const Editor = dynamic(() => import('@/components/editor/editor'), {
+  ssr: false,
+})
+
+const Home: NextPage = () => {
+  //state to hold output data. we'll use this for rendering later
+  const [data, setData] = useState<OutputData>()
+  return <Editor data={data} onChange={setData} holder="editorjs-container" />
+}
+export default Home
