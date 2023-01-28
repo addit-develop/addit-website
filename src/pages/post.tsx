f575cc067949b4b0a4e5d9ea7e555fedb5300d94
jin-su-choi <cjs33233@snu.ac.kr>
2023-01-28 13:09:19 +0900

    feat: add post page

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,7 +1,33 @@
 import Head from 'next/head'
 import { NextPage } from 'next/types'
+import { parser } from 'editorjs-viewer'
+import styles from '@/styles/post.module.css'
+
+// example data from editor.js
+const example = {
+  blocks: [
+    {
+      id: '4VO1-bae5v',
+      type: 'header',
+      data: {
+        text: 'Editor.js',
+        level: 1,
+      },
+    },
+    {
+      id: 'uF5EPBJ50g',
+      type: 'paragraph',
+      data: {
+        text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
+      },
+    },
+  ],
+}
 
 const PostPage: NextPage = () => {
+  // turn json to html
+  const result = parser.toHTML(example.blocks)
+
   return (
     <>
       <Head>
@@ -9,7 +35,9 @@ const PostPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>this is PostPage.</h1>
+        <div className={styles.page}>
+          <div className={styles.content} dangerouslySetInnerHTML={{ __html: result }} />
+        </div>
       </main>
     </>
   )
