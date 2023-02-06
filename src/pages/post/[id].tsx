210eedfc33c57f0e3621eea98d9d282540ee2b43
jin-su-choi <cjs33233@snu.ac.kr>
2023-02-06 16:23:31 +0900

    change post page to ssr

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { NextPage } from 'next/types'
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next/types'
 import styles from '@/styles/post.module.css'
 import { useRouter } from 'next/router'
 import { useCallback, useEffect } from 'react'
@@ -12,12 +12,14 @@ import { OutputData } from '@editorjs/editorjs'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
+import wrapper from '@/store/configureStore'
+import { END } from 'redux-saga'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
 const Editor = dynamic(() => import('../../components/editor/editor'), {
-  ssr: false,
+  ssr: true,
 })
 
 const PostPage: NextPage = () => {
@@ -27,7 +29,6 @@ const PostPage: NextPage = () => {
     version: '2.26.4',
   })
   const router = useRouter()
-  const { id } = router.query
   const dispatch = useDispatch()
   const { post, loadPostLoading } = useSelector((state: RootState) => state.postReducer)
   const { me } = useSelector((state: RootState) => state.userReducer)
@@ -38,12 +39,6 @@ const PostPage: NextPage = () => {
       .format('D MMM YYYY | HH:mm')
       .toString()
   }, [])
-
-  useEffect(() => {
-    if (id) {
-      dispatch(loadPostRequestAction({ ids: [id], amount: 1 }))
-    }
-  }, [id])
 
   const editPost = useCallback(() => {
     dispatch(editPostAction())
@@ -58,8 +53,8 @@ const PostPage: NextPage = () => {
       </Head>
       <main>
         <div className={styles.page}>
-          {loadPostLoading?(<div> Loading Post... </div>):(<></>)}
-          {(!loadPostLoading && post)? (
+          {loadPostLoading ? <div> Loading Post... </div> : <></>}
+          {!loadPostLoading && post ? (
             <div className={styles.postContainer} id="postContainer">
               <div className={styles.title}>{post.title}</div>
               <div className={styles.detail}>{`${
@@ -73,25 +68,40 @@ const PostPage: NextPage = () => {
               />
             </div>
           ) : (
-            <div>
-              This post does not exist or has been deleted.
-            </div>
+            <div>This post does not exist or has been deleted.</div>
           )}
-          {(!loadPostLoading && post && me === post.email)? (
+          {!loadPostLoading && post && me === post.email ? (
             <button className={styles.edit} onClick={editPost}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
-              <path
-                d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"
-                fill="#fff"
-              />
-            </svg>
-            Edit
-          </button>
-          ):(<></>)}
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
+                <path
+                  d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"
+                  fill="#fff"
+                />
+              </svg>
+              Edit
+            </button>
+          ) : (
+            <></>
+          )}
         </div>
       </main>
     </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context: GetServerSidePropsContext) => {
+    const { id } = context.query
+
+    if (id) {
+      store.dispatch(loadPostRequestAction({ ids: [id], amount: 1 }))
+    }
+
+    store.dispatch(END)
+    await store.sagaTask?.toPromise()
+
+    return { props: {} }
+  }
+)
 
 export default PostPage
