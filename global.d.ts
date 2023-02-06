175f9a07aa4614c0e3308625353bef6e65b8359b
jin-su-choi <cjs33233@snu.ac.kr>
2023-02-06 15:41:42 +0900

    fix: editorjs block import error

---
+++ global.d.ts
@@ -0,0 +1,19 @@
+declare module '@editorjs/paragraph' {
+  import Paragraph from '@editorjs/paragraph'
+  export = Paragraph
+}
+
+declare module '@editorjs/list' {
+  import List from '@editorjs/list'
+  export = List
+}
+
+declare module '@editorjs/table' {
+  import Table from '@editorjs/table'
+  export = Table
+}
+
+declare module '@editorjs/image' {
+  import ImageTool from '@editorjs/image'
+  export = ImageTool
+}
