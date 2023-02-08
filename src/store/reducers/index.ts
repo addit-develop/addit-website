3be0769c02f469b75423d67d4df69ef726ea2646
wansuwan00 <hse07088@snu.ac.kr>
2023-02-09 02:10:46 +0900

    fix:only pageReducer Hydrates

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -4,10 +4,20 @@ import postReducer from './postReducer'
 import pageReducer from './pageReducer'
 import userReducer from './userReducer'
 
+const pageReducerHydrated = (state : any, action : any) => {
+  switch (action.type) {
+    case HYDRATE:
+      console.log('HYDRATE', action)
+      return action.payload
+    default: {
+      return pageReducer
+    }
+  }
+};
 const rootReducer = combineReducers({
   userReducer,
   postReducer,
-  pageReducer,
+  pageReducerHydrated,
 })
 
 export default rootReducer
