d9a51c7b86d67d14c85b05a5cf4b7e73757b6ad8
wansuwan00 <hse07088@snu.ac.kr>
2023-02-08 23:55:24 +0900

    fix

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -83,7 +83,7 @@ const WritePage: NextPage = () => {
             (block.data.text.match(hashtagRegex) || []).map((e: string) => e.slice(1).toLowerCase())
           )
         } else if (block.type === 'image' && !mainImage && !block.data.file.url) {
-          const splitedImageUrl = decodeURIComponent(block.data.file.url).split('/')
+          const splitedImageUrl = block.data.file.url.split('/')
           const fileName = splitedImageUrl[splitedImageUrl.length - 1]
           mainImage = 'https://addit-football-s3.s3.ap-northeast-2.amazonaws.com/thumb/' + fileName
         }
