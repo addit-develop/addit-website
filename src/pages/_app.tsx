cf2750caeb4f5a3ead592771281a75c1d78c0f43
OXcarXierra <littlestein@snu.ac.kr>
2023-02-20 14:48:55 +0900

    add GA script, delete html

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppLayout from '@/layout/appLayout'
 import Head from 'next/head'
 import '@/styles/globals.css'
 import wrapper from '../store/configureStore'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
 
 interface AppProps {
   Component: React.ElementType
@@ -16,6 +18,25 @@ const App = ({ Component }: AppProps) => {
         <meta name="description" content="축구 전문 에디터" />
         <title>ADDIT for Football</title>
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <AppLayout>
         <Component />
       </AppLayout>
